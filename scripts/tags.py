03934da698a3a1008cf2b387b2b89b8163ed34e6
megvii-mge <61608041+megvii-mge@users.noreply.github.com>
2020-07-09 11:03:16 +0800

    feat(script): Add GAN tag

--- scripts/tags.py
+++ scripts/tags.py
@@ -13,7 +13,8 @@ class Tag:
 
 TAGS = [
     Tag(id="vision", name={"zh_CN": "视觉", "en_US": "Vision"}),
-    Tag(id="detection", name={"zh_CN": "识别", "en_US": "Detection"}),
-    Tag(id="classification", name={"zh_CN": "分类", "en_US": "Detection"}),
+    Tag(id="detection", name={"zh_CN": "检测", "en_US": "Detection"}),
+    Tag(id="classification", name={"zh_CN": "分类", "en_US": "Classification"}),
+    Tag(id="gan", name={"zh_CN": "生成对抗网络", "en_US": "GAN"}),
     Tag(id="nlp", name={"zh_CN": "自然语言处理", "en_US": "NLP"}),
 ]
